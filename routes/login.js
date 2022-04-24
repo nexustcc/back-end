5ea1c5edf38bda32ac9ded1a862e6132bdbfb779
Mateus Bispo Ramos <niremalu.mateus@gmail.com>
2022-04-24 02:10:04 -0300

    Atualização da rota de Login

--- routes/login.js
+++ routes/login.js
@@ -27,9 +27,8 @@ router.post('/', (req, res) => {
                 let nome
 
                 if(result != ""){
-                    let result_obj = result;
-                    idUsuario = result_obj[Object.keys(result_obj)[0]]["idUsuario"];
-                    nome = result_obj[Object.keys(result_obj)[0]]["nome"];
+                    idUsuario = result[Object.keys(result)[0]]["idUsuario"];
+                    nome = result[Object.keys(result)[0]]["nome"];
 
                     const sqlInstituicao = 'SELECT idInstituicao FROM tblInstituicao WHERE idUsuario = ?'
                     connection.query(
@@ -44,8 +43,11 @@ router.post('/', (req, res) => {
                             }
 
                             if(result != ""){
+                                let idInstituicao = result[Object.keys(result)[0]]["idInstituicao"]
+
                                 res.status(201).send({
                                     idUsuario: idUsuario,
+                                    idTipo: idInstituicao,
                                     nome: nome,
                                     tipo: 'instituição'
                                 });
@@ -64,8 +66,11 @@ router.post('/', (req, res) => {
                                         }
             
                                         if(result != ""){
+                                            let idProfessor = result[Object.keys(result)[0]]["idProfessor"]
+
                                             res.status(201).send({
                                                 idUsuario: idUsuario,
+                                                idTipo: idProfessor,
                                                 nome: nome,
                                                 tipo: 'professor'
                                             });
@@ -84,18 +89,39 @@ router.post('/', (req, res) => {
                                                     }
                         
                                                     if(result != ""){
+                                                        let idAluno = result[Object.keys(result)[0]]["idAluno"]
+
                                                         res.status(201).send({
                                                             idUsuario: idUsuario,
+                                                            idTipo: idAluno,
                                                             nome: nome,
                                                             tipo: 'aluno'
                                                         });
 
                                                     } else{
-                                                        res.status(201).send({
-                                                            idUsuario: idUsuario,
-                                                            nome: nome,
-                                                            tipo: 'avaliador'
-                                                        });
+                                                        const sqlAvaliador = 'SELECT idAvaliador FROM tblAluno WHERE idUsuario = ?'
+                                                        connection.query(
+                                                            sqlAvaliador,
+                                                            idUsuario,
+                                                            (error, result, field) => {
+                                                                if (error) {
+                                                                    return res.status(500).send({
+                                                                        error: error,
+                                                                        response: null,
+                                                                    });
+                                                                }
+
+                                                                let idAvaliador = result[Object.keys(result)[0]]["idAvaliador"]
+            
+                                                                res.status(201).send({
+                                                                    idUsuario: idUsuario,
+                                                                    idTipo: idAvaliador,
+                                                                    nome: nome,
+                                                                    tipo: 'avaliador'
+                                                                });
+                                                            });
+
+                                                        
                                                     }
                                                 });
                                         }
